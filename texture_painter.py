21fc002becee4cc76f501522c8a588feb572c69f
dylan <dylanf@btinternet.com>
2017-02-19 21:06:08 +0000

    show credits

--- texture_painter.py
+++ texture_painter.py
@@ -5,12 +5,18 @@ import os, sys; sys.path.append(os.path.dirname(bpy.data.filepath)); import text
 import importlib; importlib.reload(texture_painter); texture_painter.go()
 '''
 import codecs
+import csv
 
 def get_backers(csv_filename):
-    stream = codecs.open(csv_filename, 'r', 'utf-8-sig')
-    return stream # change to backer object later
+    with codecs.open(csv_filename, 'r', 'utf-8-sig') as stream:
+        iterable = csv.reader(stream)
+        header = next(iterable)
+        for row in iterable:
+            backer = dict(zip(header, row))
+            yield backer
 
 def go():
     print("texture painter starting up")
     # read thcsv
-    print(get_backers('backers_10.csv'))
+    for backer in get_backers('backers_10.csv'):
+        print(backer)
