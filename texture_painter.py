23f2e9f8e66db3c4ef8923db9c3f75cbfb707311
dylan <dylanf@btinternet.com>
2017-02-20 00:07:29 +0000

    write files

--- texture_painter.py
+++ texture_painter.py
@@ -7,7 +7,9 @@ import importlib; importlib.reload(texture_painter); texture_painter.go()
 import bpy
 import codecs
 import csv
+import os
 from PIL import Image, ImageFont, ImageDraw
+
 
 def get_backers(csv_filename):
     with codecs.open(csv_filename, 'r', 'utf-8-sig') as stream:
@@ -17,16 +19,19 @@ def get_backers(csv_filename):
             backer = dict(zip(header, row))
             yield backer
 
-def render_text_to_file(text_to_render):
-    image = Image.new('RGB', (128,128)) # create Image
+def render_text_to_file(text_to_render, to_filename):
+    image = Image.new('RGB', (512,64)) # create Image
     draw = ImageDraw.Draw(image) # create image-draw & font object
     fnt = ImageFont.truetype('Daily1915.ttf',50) # draw text to image-draw
     draw.text((0,0), text_to_render, font=fnt, fill=(255,255,255))
-    image.save("test2.png")# save image to file
+    image.save(to_filename)# save image to file
 
 
 def go():
     print("texture painter starting up")
-    # read thcsv
+    cwd = os.path.dirname(bpy.data.filepath)
     for backer in get_backers('backers_10.csv'):
-        render_text_to_file("wahhoo!")
+        text_to_render = backer['Name'] + ', ' + backer['Country']
+        filename = cwd + '\\texture_cache\\' + backer['Number']+ '.png'
+        print("Rendering", text_to_render, "to", filename)
+        render_text_to_file(text_to_render, filename)
